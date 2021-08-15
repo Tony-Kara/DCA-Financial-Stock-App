2e2e1a72dbc1e7482acf4637ba987d52827e8b18
Tony-Kara <eniolaanthony@gmail.com>
2021-08-15 19:05:19 +0600

    Add prototype cell, add new swift file for TableViewCell, connect all IBOutlets

--- DCA Financial Stock App/Controller/SearchTableViewController.swift
+++ DCA Financial Stock App/Controller/SearchTableViewController.swift
@@ -30,6 +30,15 @@ class SearchTableViewController: UITableViewController {
     }
     
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        return cell
+    }
+    
 
 }
 
---
+++ DCA Financial Stock App/View/SearchTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTableViewCell.swift
+//  DCA Financial Stock App
+//
+//  Created by mac on 8/15/21.
+//
+
+import UIKit
+
+
+class SearchTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var assetNameLabel: UILabel!
+    @IBOutlet weak var assetSymbolLabel: UILabel!
+    @IBOutlet weak var assetTypeLabel: UILabel!
+}
